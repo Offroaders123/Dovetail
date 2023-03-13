6ffcc924905a6fa340dd68435419d8df1504fb1d
Offroaders123 <Offroaders123@users.noreply.github.com>
2023-03-13 23:23:00 +0000

    Deploy to GitHub Pages

---
+++ dist/app.js
@@ -0,0 +1,79 @@
+import "./compression-polyfill.js";
+import { read, write, parse, stringify, NBTData } from "nbtify";
+if (window.isSecureContext) {
+    await navigator.serviceWorker.register("./service-worker.js");
+}
+const saver = document.querySelector("#saver");
+const opener = document.querySelector("#opener");
+const editor = document.querySelector("#editor");
+let config;
+let name;
+document.addEventListener("dragover", event => {
+    event.preventDefault();
+    if (event.dataTransfer === null)
+        return;
+    event.dataTransfer.dropEffect = "copy";
+});
+document.addEventListener("drop", async (event) => {
+    event.preventDefault();
+    if (event.dataTransfer === null)
+        return;
+    const items = [...event.dataTransfer.items].filter(item => item.kind === "file");
+    if (items.length === 0)
+        return;
+    const [item] = items;
+    const file = item.getAsFile();
+    await openFile(file);
+});
+saver.addEventListener("click", async () => {
+    const snbt = editor.value;
+    const nbt = new NBTData(parse(snbt), config);
+    const file = await writeFile(nbt);
+    saveFile(file);
+});
+opener.addEventListener("change", async () => {
+    if (opener.files === null)
+        return;
+    if (opener.files.length === 0)
+        return;
+    const [file] = opener.files;
+    await openFile(file);
+});
+export async function openFile(file) {
+    saver.disabled = true;
+    editor.disabled = true;
+    const nbt = await readFile(file);
+    if (nbt === null) {
+        return;
+    }
+    const snbt = stringify(nbt, { space: 2 });
+    config = nbt;
+    name = file.name;
+    saver.disabled = false;
+    editor.value = snbt;
+    editor.disabled = false;
+}
+export async function readFile(file) {
+    try {
+        const buffer = await file.arrayBuffer();
+        const nbt = await read(buffer);
+        return nbt;
+    }
+    catch (error) {
+        alert(error);
+        return null;
+    }
+}
+export function saveFile(file) {
+    const anchor = document.createElement("a");
+    const blob = URL.createObjectURL(file);
+    anchor.href = blob;
+    anchor.download = file.name;
+    anchor.click();
+    URL.revokeObjectURL(blob);
+}
+export async function writeFile(nbt) {
+    const data = await write(nbt);
+    const file = new File([data], name);
+    return file;
+}
---
+++ dist/compression-polyfill.js
@@ -0,0 +1,7 @@
+export const usePolyfill = (typeof CompressionStream === "undefined" ||
+    typeof DecompressionStream === "undefined");
+if (usePolyfill) {
+    const { CompressionStream, DecompressionStream } = await import("@stardazed/streams-compression");
+    globalThis.CompressionStream = CompressionStream;
+    globalThis.DecompressionStream = DecompressionStream;
+}
