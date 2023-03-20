be63c3fe3dcf264c1ea88bb297370084441f86e4
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-03-20 14:41:28 -0700

    Pre-Bundling Niceties
    Trying out using a bundler and possibly UI framework for the app! These were a few changes that I wanted to make regardless of going with a bundler or not.

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 /// <reference no-default-lib="true"/>
 /// <reference types="better-typescript/worker"/>
 
-const version = "Dovetail v1.2.9";
+const version = "Dovetail v1.2.10";
 
 self.addEventListener("activate",event => {
   event.waitUntil(removeOutdatedVersions());
